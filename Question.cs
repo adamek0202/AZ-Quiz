66f965f23e11e39a8ca283a27fb3b0e5036b8b7c
Adam Benda <adamek0584@gmail.com>
2025-11-04 21:23:34 +0100

    Question třída

---
+++ Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZ_Kviz
+{
+    internal class Question
+    {
+        public string Text { get; }
+        public string Answer { get; }
+        public int SetID { get; }
+
+        public Question(string text, string answer, int setID)
+        {
+            Text = text;
+            Answer = answer;
+            SetID = setID;
+        }
+    }
+}
