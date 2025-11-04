86b1120be44bf7f3f072e28048e1b8adb2e0caa6
Adam Benda <adamek0584@gmail.com>
2025-11-04 20:15:44 +0100

    EmptyDatasetException

---
+++ EmptyDatasetException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AZ_Kviz
+{
+    internal class EmptyDatasetException : Exception
+    {
+        public EmptyDatasetException() {}
+        public EmptyDatasetException(string message) : base(message) {}
+        public EmptyDatasetException(string message, Exception innerException) : base(message, innerException) {}
+    }
+}
