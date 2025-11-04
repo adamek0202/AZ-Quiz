a4f04d19217183134e92880e0b2426a8f70494ed
Adam Benda <adamek0584@gmail.com>
2025-11-04 14:47:44 +0100

    Úprava MainForm

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,12 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(606, 530);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
             this.Text = "AZ Kvíz";
             this.ResumeLayout(false);
 
--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ namespace AZ_Kvíz
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
