73dcacbe77e2e3dea93fbc87110e3758bf6c56b7
Adam Benda <adamek0584@gmail.com>
2025-11-04 20:17:53 +0100

    QuestionsSet

---
+++ QuestionsSet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZ_Kviz
+{
+    internal class QuestionsSet
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public string Scope { get; }
+        public string Author { get; }
+        public string Dufficulty { get; }
+
+        public QuestionsSet(int id, string name, string scope, string author, string dufficulty)
+        {
+            Id = id;
+            Name = name;
+            Scope = scope;
+            Author = author;
+            Dufficulty = dufficulty;
+        }
+    }
+}
