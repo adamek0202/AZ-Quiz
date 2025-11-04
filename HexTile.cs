6cbdda918dd25077bac0b94f81b4b72189f0a8e1
Adam Benda <adamek0584@gmail.com>
2025-11-04 14:47:14 +0100

    Třída HexTile

---
+++ HexTile.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace AZ_Kvíz
+{
+    internal class HexTile
+    {
+        public PointF[] Points;
+        public Color FillColor;
+        public string Label = "";
+
+        public bool HitTest(PointF p) {
+            using (var path = new GraphicsPath())
+            {
+                path.AddPolygon(Points);
+                return path.IsVisible(p);
+            }
+        }
+
+        public PointF GetCenter()
+        {
+            float x = 0, y = 0;
+            foreach (var pt in Points)
+            {
+                x += pt.X;
+                y += pt.Y;
+            }
+            return new PointF(x / Points.Length, y / Points.Length);
+        }
+
+        public void Draw(Graphics g, Font font)
+        {
+            using (var brush = new SolidBrush(FillColor))
+            using (var pen = new Pen(Color.Black, 1))
+            {
+                g.FillPolygon(brush, Points);
+                g.DrawPolygon(pen, Points);
+            }
+
+            if (!string.IsNullOrEmpty(Label))
+            {
+                var center = GetCenter();
+                var size = g.MeasureString(Label, font);
+                g.DrawString(Label, font, Brushes.Black,
+                    center.X - size.Width / 2,
+                    center.Y - size.Height / 2);
+            }
+        }
+    }
+}
