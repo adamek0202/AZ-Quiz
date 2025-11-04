7524892074e24caac96adaf62ba2c44a4af85589
Adam Benda <adamek0584@gmail.com>
2025-11-04 20:17:40 +0100

    Reorganizace formulářů

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AZ_Kviz
+﻿namespace AZ_Kviz.Forms
 {
     partial class MainForm
     {
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AZ_Kviz
+namespace AZ_Kviz.Forms
 {
     public partial class MainForm : Form
     {
