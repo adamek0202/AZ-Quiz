5c2a8b6b0aa2b3d754237a64bf88d60dfd64c33a
Adam Benda <adamek0584@gmail.com>
2025-11-05 21:01:25 +0100

    ReadOnlyTextBox

---
+++ ReadOnlyTextBox.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace AZ_Kviz
+{
+    internal class ReadOnlyTextBox : TextBox
+    {
+        public ReadOnlyTextBox()
+        {
+            SelectionHighlightEnabled = false;
+        }
+        const int WM_SETFOCUS = 0x007;
+        const int WM_KILLFOCUS = 0x0008;
+        [DefaultValue(true)]
+        public bool SelectionHighlightEnabled { get; set; }
+        protected override void DefWndProc(ref Message m)
+        {
+            if(m.Msg == WM_SETFOCUS && !SelectionHighlightEnabled)
+            {
+                m.Msg = WM_KILLFOCUS;
+            }
+            base.DefWndProc(ref m);
+        }
+    }
+}
