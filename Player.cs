0618a709ae162ae2fe90c838fe78a5549ef894ef
Adam Benda <adamek0584@gmail.com>
2025-11-06 07:28:11 +0100

    Player

---
+++ Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZ_Kviz
+{
+    internal static class Player
+    {
+        public enum Players
+        {
+            PlayerOne,
+            PlayerTwo
+        }
+        public static Players currentPlayer { get; private set; } = Players.PlayerOne;
+
+        public static void NextPlayer()
+        {
+            currentPlayer = (currentPlayer == Players.PlayerOne) ? Players.PlayerTwo : Players.PlayerOne;
+        }
+    }
+}
