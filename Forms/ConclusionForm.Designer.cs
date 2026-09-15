82c4a84d92a9bb872b090db1d15bc609cf1569b8
Adam Benda <adamek0584@gmail.com>
2026-09-15 16:56:59 +0200

    Zprovoznění ConclusionForm

--- Forms/ConclusionForm.Designer.cs
+++ Forms/ConclusionForm.Designer.cs
@@ -28,43 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.playerOneGroupBox = new System.Windows.Forms.GroupBox();
+            this.playerOneIncorrect = new AZ_Kviz.Components.ReadOnlyTextBox();
+            this.playerOneCorrect = new AZ_Kviz.Components.ReadOnlyTextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.readOnlyTextBox2 = new AZ_Kviz.Components.ReadOnlyTextBox();
-            this.readOnlyTextBox1 = new AZ_Kviz.Components.ReadOnlyTextBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.readOnlyTextBox3 = new AZ_Kviz.Components.ReadOnlyTextBox();
-            this.readOnlyTextBox4 = new AZ_Kviz.Components.ReadOnlyTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.playerTwoGroupBox = new System.Windows.Forms.GroupBox();
+            this.playerTwoCorrect = new AZ_Kviz.Components.ReadOnlyTextBox();
+            this.playerTwoIncorrect = new AZ_Kviz.Components.ReadOnlyTextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.endButton = new System.Windows.Forms.Button();
+            this.repeatButton = new System.Windows.Forms.Button();
+            this.playerOneGroupBox.SuspendLayout();
+            this.playerTwoGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // playerOneGroupBox
             // 
-            this.groupBox1.Controls.Add(this.readOnlyTextBox2);
-            this.groupBox1.Controls.Add(this.readOnlyTextBox1);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(10, 0);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(151, 100);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Hráč 1";
+            this.playerOneGroupBox.Controls.Add(this.playerOneIncorrect);
+            this.playerOneGroupBox.Controls.Add(this.playerOneCorrect);
+            this.playerOneGroupBox.Controls.Add(this.label2);
+            this.playerOneGroupBox.Controls.Add(this.label1);
+            this.playerOneGroupBox.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.playerOneGroupBox.Location = new System.Drawing.Point(11, 0);
+            this.playerOneGroupBox.Name = "playerOneGroupBox";
+            this.playerOneGroupBox.Size = new System.Drawing.Size(154, 100);
+            this.playerOneGroupBox.TabIndex = 0;
+            this.playerOneGroupBox.TabStop = false;
+            this.playerOneGroupBox.Text = "Hráč 1";
             // 
-            // label1
+            // playerOneIncorrect
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 22);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Správné";
+            this.playerOneIncorrect.BackColor = System.Drawing.Color.Red;
+            this.playerOneIncorrect.ForeColor = System.Drawing.Color.White;
+            this.playerOneIncorrect.Location = new System.Drawing.Point(98, 60);
+            this.playerOneIncorrect.Name = "playerOneIncorrect";
+            this.playerOneIncorrect.SelectionHighlightEnabled = false;
+            this.playerOneIncorrect.Size = new System.Drawing.Size(50, 29);
+            this.playerOneIncorrect.TabIndex = 1;
+            this.playerOneIncorrect.Text = "0";
+            // 
+            // playerOneCorrect
+            // 
+            this.playerOneCorrect.BackColor = System.Drawing.Color.Lime;
+            this.playerOneCorrect.Location = new System.Drawing.Point(98, 22);
+            this.playerOneCorrect.Name = "playerOneCorrect";
+            this.playerOneCorrect.SelectionHighlightEnabled = false;
+            this.playerOneCorrect.Size = new System.Drawing.Size(50, 29);
+            this.playerOneCorrect.TabIndex = 1;
+            this.playerOneCorrect.Text = "0";
             // 
             // label2
             // 
@@ -75,60 +88,48 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Špatné";
             // 
-            // readOnlyTextBox2
+            // label1
             // 
-            this.readOnlyTextBox2.BackColor = System.Drawing.Color.Red;
-            this.readOnlyTextBox2.ForeColor = System.Drawing.Color.White;
-            this.readOnlyTextBox2.Location = new System.Drawing.Point(98, 57);
-            this.readOnlyTextBox2.Name = "readOnlyTextBox2";
-            this.readOnlyTextBox2.SelectionHighlightEnabled = false;
-            this.readOnlyTextBox2.Size = new System.Drawing.Size(40, 29);
-            this.readOnlyTextBox2.TabIndex = 1;
-            this.readOnlyTextBox2.Text = "0";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Správné";
             // 
-            // readOnlyTextBox1
+            // playerTwoGroupBox
             // 
-            this.readOnlyTextBox1.BackColor = System.Drawing.Color.Lime;
-            this.readOnlyTextBox1.Location = new System.Drawing.Point(98, 22);
-            this.readOnlyTextBox1.Name = "readOnlyTextBox1";
-            this.readOnlyTextBox1.SelectionHighlightEnabled = false;
-            this.readOnlyTextBox1.Size = new System.Drawing.Size(40, 29);
-            this.readOnlyTextBox1.TabIndex = 1;
-            this.readOnlyTextBox1.Text = "0";
+            this.playerTwoGroupBox.Controls.Add(this.playerTwoCorrect);
+            this.playerTwoGroupBox.Controls.Add(this.playerTwoIncorrect);
+            this.playerTwoGroupBox.Controls.Add(this.label3);
+            this.playerTwoGroupBox.Controls.Add(this.label4);
+            this.playerTwoGroupBox.Location = new System.Drawing.Point(171, 0);
+            this.playerTwoGroupBox.Name = "playerTwoGroupBox";
+            this.playerTwoGroupBox.Size = new System.Drawing.Size(155, 100);
+            this.playerTwoGroupBox.TabIndex = 0;
+            this.playerTwoGroupBox.TabStop = false;
+            this.playerTwoGroupBox.Text = "Hráč 2";
             // 
-            // groupBox2
+            // playerTwoCorrect
             // 
-            this.groupBox2.Controls.Add(this.readOnlyTextBox3);
-            this.groupBox2.Controls.Add(this.readOnlyTextBox4);
-            this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Location = new System.Drawing.Point(167, 0);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(149, 100);
-            this.groupBox2.TabIndex = 0;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Hráč 2";
+            this.playerTwoCorrect.BackColor = System.Drawing.Color.Lime;
+            this.playerTwoCorrect.Location = new System.Drawing.Point(98, 25);
+            this.playerTwoCorrect.Name = "playerTwoCorrect";
+            this.playerTwoCorrect.SelectionHighlightEnabled = false;
+            this.playerTwoCorrect.Size = new System.Drawing.Size(50, 29);
+            this.playerTwoCorrect.TabIndex = 1;
+            this.playerTwoCorrect.Text = "0";
             // 
-            // readOnlyTextBox3
+            // playerTwoIncorrect
             // 
-            this.readOnlyTextBox3.BackColor = System.Drawing.Color.Red;
-            this.readOnlyTextBox3.ForeColor = System.Drawing.Color.White;
-            this.readOnlyTextBox3.Location = new System.Drawing.Point(98, 57);
-            this.readOnlyTextBox3.Name = "readOnlyTextBox3";
-            this.readOnlyTextBox3.SelectionHighlightEnabled = false;
-            this.readOnlyTextBox3.Size = new System.Drawing.Size(40, 29);
-            this.readOnlyTextBox3.TabIndex = 1;
-            this.readOnlyTextBox3.Text = "0";
-            // 
-            // readOnlyTextBox4
-            // 
-            this.readOnlyTextBox4.BackColor = System.Drawing.Color.Lime;
-            this.readOnlyTextBox4.Location = new System.Drawing.Point(98, 22);
-            this.readOnlyTextBox4.Name = "readOnlyTextBox4";
-            this.readOnlyTextBox4.SelectionHighlightEnabled = false;
-            this.readOnlyTextBox4.Size = new System.Drawing.Size(40, 29);
-            this.readOnlyTextBox4.TabIndex = 1;
-            this.readOnlyTextBox4.Text = "0";
+            this.playerTwoIncorrect.BackColor = System.Drawing.Color.Red;
+            this.playerTwoIncorrect.ForeColor = System.Drawing.Color.White;
+            this.playerTwoIncorrect.Location = new System.Drawing.Point(98, 57);
+            this.playerTwoIncorrect.Name = "playerTwoIncorrect";
+            this.playerTwoIncorrect.SelectionHighlightEnabled = false;
+            this.playerTwoIncorrect.Size = new System.Drawing.Size(50, 29);
+            this.playerTwoIncorrect.TabIndex = 1;
+            this.playerTwoIncorrect.Text = "0";
             // 
             // label3
             // 
@@ -148,38 +149,41 @@
             this.label4.TabIndex = 0;
             this.label4.Text = "Správné";
             // 
-            // button1
+            // endButton
             // 
-            this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(216, 106);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 35);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Ukončit";
-            this.button1.UseVisualStyleBackColor = false;
+            this.endButton.BackColor = System.Drawing.Color.Red;
+            this.endButton.ForeColor = System.Drawing.Color.White;
+            this.endButton.Location = new System.Drawing.Point(226, 106);
+            this.endButton.Name = "endButton";
+            this.endButton.Size = new System.Drawing.Size(100, 35);
+            this.endButton.TabIndex = 1;
+            this.endButton.Text = "Ukončit";
+            this.endButton.UseVisualStyleBackColor = false;
+            this.endButton.Click += new System.EventHandler(this.EndButton_Click);
             // 
-            // button2
+            // repeatButton
             // 
-            this.button2.BackColor = System.Drawing.Color.SteelBlue;
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(10, 106);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 35);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Opakovat";
-            this.button2.UseVisualStyleBackColor = false;
+            this.repeatButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.repeatButton.ForeColor = System.Drawing.Color.White;
+            this.repeatButton.Location = new System.Drawing.Point(11, 106);
+            this.repeatButton.Name = "repeatButton";
+            this.repeatButton.Size = new System.Drawing.Size(100, 35);
+            this.repeatButton.TabIndex = 1;
+            this.repeatButton.Text = "Opakovat";
+            this.repeatButton.UseVisualStyleBackColor = false;
+            this.repeatButton.Visible = false;
+            this.repeatButton.Click += new System.EventHandler(this.RepeatButton_Click);
             // 
             // ConclusionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(327, 153);
+            this.ClientSize = new System.Drawing.Size(329, 145);
             this.ControlBox = false;
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.repeatButton);
+            this.Controls.Add(this.endButton);
+            this.Controls.Add(this.playerTwoGroupBox);
+            this.Controls.Add(this.playerOneGroupBox);
             this.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
@@ -191,27 +195,27 @@
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Výsledky hry";
             this.TopMost = true;
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
+            this.playerOneGroupBox.ResumeLayout(false);
+            this.playerOneGroupBox.PerformLayout();
+            this.playerTwoGroupBox.ResumeLayout(false);
+            this.playerTwoGroupBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox playerOneGroupBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private Components.ReadOnlyTextBox readOnlyTextBox2;
-        private Components.ReadOnlyTextBox readOnlyTextBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private Components.ReadOnlyTextBox readOnlyTextBox3;
-        private Components.ReadOnlyTextBox readOnlyTextBox4;
+        private Components.ReadOnlyTextBox playerOneIncorrect;
+        private Components.ReadOnlyTextBox playerOneCorrect;
+        private System.Windows.Forms.GroupBox playerTwoGroupBox;
+        private Components.ReadOnlyTextBox playerTwoIncorrect;
+        private Components.ReadOnlyTextBox playerTwoCorrect;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button endButton;
+        private System.Windows.Forms.Button repeatButton;
     }
 }
--- Forms/ConclusionForm.cs
+++ Forms/ConclusionForm.cs
@@ -4,9 +4,32 @@ namespace AZ_Kviz.Forms
 {
     public partial class ConclusionForm : Form
     {
+        public bool RepeatGame { get; private set; } = false;
         public ConclusionForm()
         {
             InitializeComponent();
+            FillData();
+        }
+
+        private void FillData()
+        {
+            playerOneCorrect.Text = Game.PlayerOne.Correct.ToString();
+            playerOneIncorrect.Text = Game.PlayerOne.Incorrect.ToString();
+            playerTwoCorrect.Text = Game.PlayerTwo.Correct.ToString();
+            playerTwoIncorrect.Text = Game.PlayerTwo.Incorrect.ToString();
+            playerOneGroupBox.Text = Game.PlayerOne.Name;
+            playerTwoGroupBox.Text = Game.PlayerTwo.Name;
+        }
+
+        private void EndButton_Click(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+
+        private void RepeatButton_Click(object sender, System.EventArgs e)
+        {
+            RepeatGame = true;
+            Close();
         }
     }
 }
